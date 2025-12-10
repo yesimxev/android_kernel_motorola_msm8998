e74742a52a4b3fea5e1b7047f00333b163216825
yesimxev <yesimxev@github.com>
2025-12-10 00:09:36 +0000

    Fixed rtl8812au driver (IEEE80211_BAND_xx)

--- drivers/net/wireless/realtek/rtl8812au/os_dep/osdep_service.c
+++ drivers/net/wireless/realtek/rtl8812au/os_dep/osdep_service.c
@@ -3071,12 +3071,13 @@ inline BOOLEAN is_eol(char c)
 * Return	TRUE if c is represent for space
 *		FALSE otherwise.
 */
+
 inline BOOLEAN is_space(char c)
 {
-	if (c == ' ' || c == '\t')
-		return _TRUE;
-	else
-		return _FALSE;
+        if (c == '\r' || c == ' ')
+                return _TRUE;
+        else
+                return _FALSE;
 }
 
 /**
